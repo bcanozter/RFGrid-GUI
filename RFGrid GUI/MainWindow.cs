6ad88aa664211697ff19fc4f02c7980badb80bae
bcanozter <burak.ozter@dal.ca>
2020-02-05 17:18:40 -0800

    test commit

--- RFGrid GUI/MainWindow.cs
+++ RFGrid GUI/MainWindow.cs
@@ -19,6 +19,7 @@ namespace Tag_Scanner
         {
             //this.BackgroundImage = Properties.Resources.im; #disable until we find a better background image
             InitializeComponent();
+            int test = 123;
         }
 
 
